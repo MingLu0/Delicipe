9ff5dc46748ca0fa8376c01d7d68852324b0e9b4
Ming Luo <luoming886@gmail.com>
2019-03-18 13:08:22 +1300

    finished tabbed functionality

--- app/src/main/java/com/luo/ming/delicipe/MainActivity.java
+++ app/src/main/java/com/luo/ming/delicipe/MainActivity.java
@@ -1,13 +1,31 @@
 package com.luo.ming.delicipe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button browseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        browseButton = findViewById(R.id.browseButton);
+
+        browseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //presenter.login();  ONLY LINE BEFORE EDITING
+                // Go to Home Page activity
+                Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
